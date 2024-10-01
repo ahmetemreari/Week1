875855b12a4e05dcd230faeba1276524031fa889
Emre <Emre@DESKTOP-6HFUN0S>
2024-10-01 12:58:42 +0300

    Proje dosyası ekle.

---
+++ helloworld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Emre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahmet Emre Arı  \nKendimi yazılım sektöründe gelistirip kariyer yapmak.");
+        }
+    }
+}
